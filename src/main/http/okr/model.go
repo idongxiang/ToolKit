233935d1bc6aced8ac66ab8526ddc01413942f3e
董翔 <idongxiang@qq.com>
2019-09-19 01:28:57 +0800

    query demo

---
+++ src/main/http/okr/model.go
@@ -0,0 +1,19 @@
+package main
+
+type SkyNetLog struct {
+	appid        string
+	logtime      string
+	msg          string
+	extrainfo    string
+	module       string
+	category     string
+	sub_category string
+	priority     string
+	ip           string
+	filter1      string
+	filter2      string
+	contextid    string
+	domainname   string
+	dt           string
+	hour         string
+}
